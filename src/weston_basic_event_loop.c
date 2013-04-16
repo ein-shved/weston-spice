0159a96967d33524d14246b12427de204dd59394
Shved <shved@lvk.cs.msu.su>
2013-04-16 14:17:03 +0400

    weston-spice: copyright added and shared cleared

--- src/weston_basic_event_loop.c
+++ src/weston_basic_event_loop.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright © 2013 Yury Shvedov <shved@lvk.cs.msu.su>
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and
+ * its documentation for any purpose is hereby granted without fee, provided
+ * that the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of the copyright holders not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.  The copyright holders make
+ * no representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
@@ -8,7 +30,6 @@
 #include <spice/macros.h>
 #include "weston_basic_event_loop.h"
 #include "compositor-spice.h"
-#include "shared/ring.h"
 
 #define DPRINTF(fmt, ...) \
     dprint (4, fmt, ## __VA_ARGS__)
